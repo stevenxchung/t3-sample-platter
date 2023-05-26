2e28ee8db5534fbbe4668a22d5a910fc2c971b95
Steven <stevenxchung@gmail.com>
2023-05-26 00:37:44 -0400

    Add profile page and router

---
+++ src/pages/profile/[slug].tsx
@@ -0,0 +1,71 @@
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import { api } from "~/utils/api";
+
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import Image from "next/image";
+import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
+  });
+
+  if (!data) return <div>404</div>;
+
+  return (
+    <>
+      <Head>
+        <title>{data.username}</title>
+      </Head>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile picture`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("No slug!");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: ["/profile/@stevenxchung"], fallback: "blocking" };
+};
+
+export default ProfilePage;
